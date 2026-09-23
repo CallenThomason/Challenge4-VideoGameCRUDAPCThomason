9e0e4ceae89032ba10173d6b8c9836e7763927f8
CallenThomason <thomasoncallen@gmail.com>
2026-09-23 14:45:26 -0700

    Fixed the bug where you couldnt search the genres with a '/' in them

--- Services/GameServices.cs
+++ Services/GameServices.cs
@@ -7,16 +7,16 @@ namespace Challenge4_VideoGameCRUDAPCThomason.Services
     public class GameServices : IGameServices
     {
         private static List<GameSelection> _videoGames = [
-            new GameSelection{Id = 1, Title = "God Of War", Rating = "M", Genre = "Action/Adventure", IsAvailable = true },
-            new GameSelection{Id = 2, Title = "Spider Man", Rating = "T", Genre = "Action/Adventure", IsAvailable = true },
+            new GameSelection{Id = 1, Title = "God Of War", Rating = "M", Genre = "Action&Adventure", IsAvailable = true },
+            new GameSelection{Id = 2, Title = "Spider Man", Rating = "T", Genre = "Action&Adventure", IsAvailable = true },
             new GameSelection{Id = 3, Title = "Marvel Rivals", Rating = "M", Genre = "PvP", IsAvailable = true },
             new GameSelection{Id = 4, Title = "Pokemon Red", Rating = "E", Genre = "RPG", IsAvailable = false },
             new GameSelection{Id = 5, Title = "Fortnite", Rating = "T", Genre = "Shooter", IsAvailable = true },
-            new GameSelection{Id = 6, Title = "Minecraft", Rating = "E", Genre = "Action/Adventure", IsAvailable = false },
+            new GameSelection{Id = 6, Title = "Minecraft", Rating = "E", Genre = "Action&Adventure", IsAvailable = false },
             new GameSelection{Id = 7, Title = "Enter the Gungeon", Rating = "T", Genre = "Rougelike", IsAvailable = true },
             new GameSelection{Id = 8, Title = "Call Of Duty Black Ops III", Rating = "M", Genre = "Shooter", IsAvailable = false },
             new GameSelection{Id = 9, Title = "Pokemon Legends ZA", Rating = "E", Genre = "RPG", IsAvailable = true },
-            new GameSelection{Id = 10, Title = "Jedi: Fallen Order", Rating = "T", Genre = "Action/Adventure", IsAvailable = false }
+            new GameSelection{Id = 10, Title = "Jedi: Fallen Order", Rating = "T", Genre = "Action&Adventure", IsAvailable = false }
 
         ];
         static int newId = 11; 
